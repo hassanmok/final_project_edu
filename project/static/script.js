52dd1034d1e853b55cde3a23d2745ef2ece69851
Hassan abdelhadi kadhim <hasan.abd21031a@kecbu.uobaghdad.edu.iq>
2024-10-28 02:42:19 +0300

    hassan edit

---
+++ project/static/script.js
@@ -0,0 +1,22 @@
+
+setTimeout(function() {
+    document.getElementById("loading-screen").style.display = "none";
+    document.getElementById("main").style.display = "block";
+    
+    }, 2700);
+
+var typed = new Typed("#text_mok", {
+    strings: ["Join us to<br>learn program like <span style='color: rgb(255, 255, 255);'>spider web.</span>"],
+    typeSpeed: 50,
+    showCursor: false
+  });
+  
+  var typed2 = new Typed("#text_mok2", {
+    strings: ["<b><span style='color: #0116FF;'>#include</span>&lt;<span style='color: #F31BAB;'>iostream</span>&gt;<br> \
+               using namespace <span style='color: #0116FF;'>std;</span><br> \
+               <span style='color: #0116FF;'>int</span> main() {<br> \
+               // <span style='color: #00FFE1;'>Welcome message for<br>Spider Coding visitors</span><br> \
+               cout << <span style='color: #EEFF00;'>\"Hi! Welcome to Spider Coding!\"</span> << endl;<br> \
+               <span style='color: #0116FF;'>return</span> 0; }</b>"],
+    typeSpeed: 25
+  });
