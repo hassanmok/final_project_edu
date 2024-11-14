f8dce22fe13f422fa043679e36f021c82adb98a1
Hassan abdelhadi kadhim <hasan.abd21031a@kecbu.uobaghdad.edu.iq>
2024-11-14 14:46:02 +0300

    hassan edit

--- project/edu_app/urls.py
+++ project/edu_app/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("challenges", views.challenges, name="challenges"),
     path("login", views.user_login, name="login"),
     path("signup", views.signup, name="signup"),
-    path("logout", views.user_logout, name="logout")
+    path("logout", views.user_logout, name="logout"),
+    path("profile", views.profile, name="profile")
 ]
